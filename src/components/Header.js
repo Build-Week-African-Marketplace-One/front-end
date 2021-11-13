b2f0e47126ac9ebf777c01a2be98e392642c480c
Vernon Bengtson <87542373+VABIII@users.noreply.github.com>
2021-11-13 01:15:47 -0600

    more of the skeleton setup and a little styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/Header.css";
 
 export default function Header() {
 
@@ -7,7 +8,9 @@ export default function Header() {
     return(
         <div className="header">
             <nav className="header-navs">
-                <Link to="/" />
+                <Link to="/" style={{ textDecoration: 'none', color: 'lightgrey' }}  >Home</Link>
+                <Link to="/login" style={{ textDecoration: 'none', color: 'lightgrey' }}>Login</Link>
+                <Link to="/logout" style={{ textDecoration: 'none', color: 'lightgrey' }}>Logout</Link>
             </nav>
         </div>
     )
